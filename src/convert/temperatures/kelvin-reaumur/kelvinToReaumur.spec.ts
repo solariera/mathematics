55cd961bb03a07e74ca47550657c16b8cce0f6a5
Always <alwaysrusting@gmail.com>
2021-06-27 21:40:07 +0900

    000004 [fix] test files

--- src/convert/temperatures/kelvin-reaumur/kelvinToReaumur.spec.ts
+++ src/convert/temperatures/kelvin-reaumur/kelvinToReaumur.spec.ts
@@ -2,10 +2,10 @@ import { tests, TestData } from '@solariera/easy-jest/lib';
 import { kelvinToReaumur as fn } from '.';
 
 const data: TestData<typeof fn>[] = [
-  { id: '0K -> レミオミュール度', params: [0], ret: -90.1395 },
-  { id: '1K -> レミオミュール度', params: [1], ret: -89.8095 },
-  { id: '10K -> レミオミュール度', params: [10], ret: -86.8395 },
-  { id: '100K -> レミオミュール度', params: [100], ret: -57.1395 },
+  { id: '0K -> レミオミュール度', params: [0], ret: -218.51999999999998 },
+  { id: '1K -> レミオミュール度', params: [1], ret: -217.72 },
+  { id: '10K -> レミオミュール度', params: [10], ret: -210.51999999999998 },
+  { id: '100K -> レミオミュール度', params: [100], ret: -138.51999999999998 },
 ];
 
 tests(fn, data);
--- src/convert/temperatures/kelvin-roemer/kelvinToRoemer.spec.ts
+++ src/convert/temperatures/kelvin-roemer/kelvinToRoemer.spec.ts
@@ -2,10 +2,10 @@ import { tests, TestData } from '@solariera/easy-jest/lib';
 import { kelvinToRoemer as fn } from '.';
 
 const data: TestData<typeof fn>[] = [
-  { id: '0K -> レーマー度', params: [0], ret: -90.1395 },
-  { id: '1K -> レーマー度', params: [1], ret: -89.8095 },
-  { id: '10K -> レーマー度', params: [10], ret: -86.8395 },
-  { id: '100K -> レーマー度', params: [100], ret: -57.1395 },
+  { id: '0K -> レーマー度', params: [0], ret: -135.90375 },
+  { id: '1K -> レーマー度', params: [1], ret: -135.37875 },
+  { id: '10K -> レーマー度', params: [10], ret: -130.65375 },
+  { id: '100K -> レーマー度', params: [100], ret: -83.40374999999999 },
 ];
 
 tests(fn, data);
